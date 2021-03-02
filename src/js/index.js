5bbea99659cc3d8db9fd6b31452af1f0412cd0a1
Flora-Pvt <f.preuvot@live.fr>
2021-03-02 15:17:36 -0500

    tags animation placement

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,80 @@
 ScrollTrigger.defaults({
-  toggleActions: "play pause resume reset",
-  scrub: 0.5,
-  start: "top 300px",
-  end: "-=400px",
-  // markers: true,
+  toggleActions: "play none none reverse",
+  scrub: 1,
+  start: "10% bottom",
+  end: "-=60%",
+  markers: true,
 });
 
 const signs = document.querySelectorAll(".sign")
-console.log(signs);
+const accoladeEnd = document.querySelectorAll("#accolade-end")
+const accolade = document.querySelectorAll("#accolade")
+const bracket = document.querySelectorAll("#bracket")
+const equal = document.querySelectorAll("#equal")
+const parenthesis = document.querySelectorAll("#parenthesis")
+const parenthesisEnd = document.querySelectorAll("#parenthesis-end")
+const semicolon = document.querySelectorAll("#semicolon")
+const tag = document.querySelectorAll("#tag")
+const tagEnd = document.querySelectorAll("#tag-end")
 
 gsap.to(signs, {
   scrollTrigger: "#work1",
-  y: 600,
-  ease: "power1.inOut",
+  translateY: 600,
+  ease: "bounce.out",
+  delay: 3,
 });
-
 gsap.to(signs, {
   scrollTrigger: {
     trigger: "#work2",
   },
-  y: 1600,
-  ease: "power1.inOut",
+  translateY: 1600,
+  ease: "bounce.out",
+  delay: 3,
 });
-
 gsap.to(signs, {
   scrollTrigger: "#work3",
-  y: 2600,
-  ease: "power1.inOut",
+  translateY: 2600,
+  ease: "bounce.out",
+  delay: 3,
+});
+
+gsap.to(tag, {
+  scrollTrigger: "#work1",
+  translateY: "108vh",
+  translateX: "-4vw",
+  ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(tagEnd, {
+  scrollTrigger: "#work1",
+  translateY: "100vh",
+  ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(tag, {
+  scrollTrigger: "#work2",
+  translateY: "190vh",
+  ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(tagEnd, {
+  scrollTrigger: "#work2",
+  translateY: "198vh",
+  translateX: "-60vw",
+  ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(tag, {
+  scrollTrigger: "#work3",
+  translateY: "348vh",
+  translateX: "-4vw",
+  ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(tagEnd, {
+  scrollTrigger: "#work3",
+  translateY: "318vh",
+  translateX: "-28vw",
+  ease: "bounce.out",
+  delay: 3,
 });
