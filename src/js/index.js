4a1c618cbd130cd969e3e7f1d0a6a37f1250c6ea
Flora-Pvt <f.preuvot@live.fr>
2021-03-03 16:20:08 -0500

    eyes animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
 ScrollTrigger.defaults({
   toggleActions: "play none none reverse",
   scrub: 1,
-  start: "10% bottom",
-  end: "-=60%",
+  start: "-30% top",
+  end: "70% bottom",
   markers: true,
 });
 
-const signs = document.querySelectorAll(".sign")
-const accoladeEnd = document.querySelectorAll("#accolade-end")
-const accolade = document.querySelectorAll("#accolade")
-const bracket = document.querySelectorAll("#bracket")
-const equal = document.querySelectorAll("#equal")
-const parenthesis = document.querySelectorAll("#parenthesis")
-const parenthesisEnd = document.querySelectorAll("#parenthesis-end")
-const semicolon = document.querySelectorAll("#semicolon")
-const tag = document.querySelector("#tag")
-const tagEnd = document.querySelector("#tag-end")
+const signs = document.querySelectorAll(".sign");
+const eyes = document.querySelectorAll(".eyes");
 
-console.log(tag);
+const accoladeEnd = document.querySelectorAll("#accolade-end");
+const accolade = document.querySelectorAll("#accolade");
+const bracket = document.querySelectorAll("#bracket");
+const equal = document.querySelectorAll("#equal");
+const parenthesis = document.querySelectorAll("#parenthesis");
+const parenthesisEnd = document.querySelectorAll("#parenthesis-end");
+const semicolon = document.querySelectorAll("#semicolon");
+const tag = document.querySelector("#tag");
+const tagEnd = document.querySelector("#tag-end");
+const animationTag = document.querySelector("#animation-eyes");
+
+console.log(tagEnd, eyes);
 
 gsap.to(signs, {
   scrollTrigger: "#work1",
-  translateY: 600,
+  translateY: "120vh",
   ease: "bounce.out",
   delay: 3,
 });
@@ -29,13 +32,13 @@ gsap.to(signs, {
   scrollTrigger: {
     trigger: "#work2",
   },
-  translateY: 1600,
+  translateY: "240vh",
   ease: "bounce.out",
   delay: 3,
 });
 gsap.to(signs, {
   scrollTrigger: "#work3",
-  translateY: 2600,
+  translateY: "350vh",
   ease: "bounce.out",
   delay: 3,
 });
@@ -43,7 +46,6 @@ gsap.to(signs, {
 gsap.to(tag, {
   scrollTrigger: "#work1",
   translateY: "108vh",
-  translateX: "-4vw",
   ease: "bounce.out",
   delay: 3,
 });
@@ -53,6 +55,12 @@ gsap.to(tagEnd, {
   ease: "bounce.out",
   delay: 3,
 });
+gsap.to(eyes, {
+  scrollTrigger: "#work1",
+  opacity: 1,
+  delay: 3,
+});
+
 gsap.to(tag, {
   scrollTrigger: "#work2",
   translateY: "190vh",
@@ -66,6 +74,12 @@ gsap.to(tagEnd, {
   ease: "bounce.out",
   delay: 3,
 });
+gsap.to(eyes, {
+  scrollTrigger: "#work2",
+  opacity: 0,
+  delay: 3,
+});
+
 gsap.to(tag, {
   scrollTrigger: "#work3",
   translateY: "348vh",
@@ -78,5 +92,10 @@ gsap.to(tagEnd, {
   translateY: "318vh",
   translateX: "-28vw",
   ease: "bounce.out",
+  delay: 3,
+});
+gsap.to(eyes, {
+  scrollTrigger: "#work3",
+  opacity: 1,
   delay: 3,
 });
