f92c5c83e62a1eb1b358323a6ce8cef6eb0f15b7
Flora-Pvt <f.preuvot@live.fr>
2021-03-20 18:57:22 -0400

    new buttons + nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const banner = document.querySelector("#show");
 
 window.addEventListener("wheel", function changeBannerVisibility() {
-  if (banner.getBoundingClientRect().y < 140) {
+  if (banner.getBoundingClientRect().y < 15) {
     banner.style.visibility = "visible";
   } else {
     banner.style.visibility = "hidden";
