8a7fad0d3e9466bbd8ff95c6e1f7fb4332bc41ca
Flora-Pvt <f.preuvot@live.fr>
2021-03-21 17:30:24 -0400

    modals setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
-const banner = document.querySelector("#show");
+const banner = document.querySelector("#banner");
 const arrow = document.querySelector("#arrow");
+
+const buttons = Array.from(document.querySelectorAll(".button"));
+const modals = Array.from(document.querySelectorAll(".modal"));
+const images = Array.from(document.querySelectorAll(".modal__img"));
 
 window.addEventListener("wheel", function changeBannerVisibility() {
   if (arrow.getBoundingClientRect().top < -200) {
@@ -8,3 +12,18 @@ window.addEventListener("wheel", function changeBannerVisibility() {
     banner.style.visibility = "hidden";
   }
 });
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    console.log(images[i - 1].style);    
+    modals[i - 1].style.opacity = 1;
+    console.log(modals[i - 1].style.opacity);
+    /*anime({
+      targets: images[i - 1],
+      duration: 500,
+      easing: "easeInOutQuad",
+      width: window.innerWidth / 2,
+      height: window.innerHeight,
+    });*/
+  });
+}
