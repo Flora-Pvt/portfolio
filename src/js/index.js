9b3f380c396c2e26dae9bec2d36436cd07ecf1c7
Flora-Pvt <f.preuvot@live.fr>
2021-03-27 11:30:16 -0400

    close modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
+const svg = document.querySelector(".morph");
+
 const banner = document.querySelector("#banner");
 const arrow = document.querySelector("#arrow");
 
-const buttons = Array.from(document.querySelectorAll(".button"));
+const openButtons = Array.from(document.querySelectorAll(".open-button"));
 const modals = Array.from(document.querySelectorAll(".modal"));
 const images = Array.from(document.querySelectorAll(".modal__img"));
-
-const svg = document.querySelector(".morph");
+const closeButtons = Array.from(document.querySelectorAll(".close-button"));
 
 window.addEventListener("wheel", function changeBannerVisibility() {
   if (arrow.getBoundingClientRect().top < -200) {
@@ -15,8 +16,8 @@ window.addEventListener("wheel", function changeBannerVisibility() {
   }
 });
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", () => {
+for (let i = 0; i < openButtons.length; i++) {
+  openButtons[i].addEventListener("click", function displayModal() {
     anime({
       targets: svg,
       scaleX: 4.5,
@@ -29,8 +30,54 @@ for (let i = 0; i < buttons.length; i++) {
       targets: modals[i - 1],
       duration: 500,
       delay: 800,
-      easing: "easeInOutQuad",
+      easing: "easeInOutQuad",      
       opacity: 1,
     });
+
+    modals[i - 1].style.pointerEvents = "all"
+  });
+}
+
+for (let i = 0; i < openButtons.length; i++) {
+  openButtons[i].addEventListener("click", function displayModal() {
+    anime({
+      targets: svg,
+      scaleX: 4.5,
+      scaleY: 3,
+      duration: 1000,
+      easing: "easeInOutQuad",
+    });
+
+    anime({
+      targets: modals[i - 1],
+      duration: 500,
+      delay: 800,
+      easing: "easeInOutQuad",      
+      opacity: 1,
+    });
+
+    modals[i - 1].style.pointerEvents = "all"
+  });
+}
+
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener("click", function closeModal() {
+    anime({
+      targets: modals[i],
+      duration: 500,
+      delay: 800,
+      easing: "easeInOutQuad",      
+      opacity: 0,
+    });
+
+    anime({
+      targets: svg,
+      scaleX: 1,
+      scaleY: 1,
+      duration: 1000,
+      easing: "easeInOutQuad",
+    });
+
+    modals[i ].style.pointerEvents = "none"
   });
 }
